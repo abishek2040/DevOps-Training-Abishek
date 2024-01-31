270cd7d04bcab94f9cc64a20260237e4f9cf9214
abishek2040 <abishek2040@proton.me>
2024-01-31 16:59:49 +0000

    Added a new function to add 2 numbers.

--- first.py
+++ first.py
@@ -1,2 +1,10 @@
 # This is a first python file on my git repo. 
 print("Hello World!")
+
+def add_function(num1, num2):
+  """A simple function that prints out the sum of 2 numbers in python."""
+  addition = num1 + num2 
+  return addition
+
+final = add_funtion(1,1)
+print(final)
